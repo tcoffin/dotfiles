e560bf0d3c6c1799b3147a57807aef8999e322f8
Tyler Coffin <tcoffin@gmail.com>
2011-11-27 00:02:25 -0500

    it installs and doesn't destroygit statusgit statusgit status

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-dotfilesWD=$PWD/`dirname $0`
+bakSuffix=.bak-dotfiles
+linksWD=$PWD/`dirname $0`/links
+
 
 # iterates through the files in this top-level directory and create the necessary symbolic links in home
-for i in `ls $dotfilesWD`
+for i in `ls $linksWD`
 do
 	file=`basename $i`
-	echo "ln -s $dotfilesWD/$file ~/.$file"
+
+	# if the destination already exists, rename it as a backup
+	if [ -e ~/.$file ]; then
+		if [ ! -e ~/.$file$bakSuffix ]; then
+			mv ~/.$file ~/.$file$bakSuffix
+		else
+			continue
+		fi
+	fi
+
+	ln -s $linksWD/$file ~/.$file
 done
